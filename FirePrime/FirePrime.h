a50cebc527c1e0492bf88fbf0f5df3fbb36b9f4f
Perceval FARAMAZ <perfaram@windowslive.com>
2016-05-29 08:48:27 +0200

    Initial Commit

---
+++ FirePrime/FirePrime.h
@@ -0,0 +1,19 @@
+//
+//  FirePrime.h
+//  FirePrime
+//
+//  Created by Perceval FARAMAZ on 29.05.16.
+//  Copyright © 2016 Faramaz. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FirePrime.
+FOUNDATION_EXPORT double FirePrimeVersionNumber;
+
+//! Project version string for FirePrime.
+FOUNDATION_EXPORT const unsigned char FirePrimeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirePrime/PublicHeader.h>
+
+
